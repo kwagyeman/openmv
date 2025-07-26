bf34ae37905612d4bf4477c40c3160e0aeb2e0b7
Kwabena W. Agyeman <kwagyeman@live.com>
2025-07-25 19:12:51 -0700

    modules/py_ml: Optimize threshold using Helium.
    The majority of the time spent processing the model is during
    the max, threshold, and nonzero operation. Combing these togheter
    in C using Helium provides a massive speedup for larger arrays.

--- scripts/libraries/ml/ml/postprocessing.py
+++ scripts/libraries/ml/ml/postprocessing.py
@@ -27,6 +27,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from ml.utils import NMS
+import uml
+import ml.utils
 from micropython import const
 from ulab import numpy as np
 
@@ -46,15 +48,9 @@ def mod(a, b):
     return a - (b * (a // b))
 
 
-def threshold(scores, threshold, scale, find_max=False, find_max_axis=1):
-    if scale > 0:
-        if find_max:
-            scores = np.max(scores, axis=find_max_axis)
-        return np.nonzero(scores > threshold)[0]
-    else:
-        if find_max:
-            scores = np.min(scores, axis=find_max_axis)
-        return np.nonzero(scores < threshold)[0]
+def softmax(x):
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return e_x / np.sum(e_x, axis=1, keepdims=True)
 
 
 def quantize(model, value, index=0):
@@ -82,15 +78,15 @@ class fomo_postprocess:
 
     def __call__(self, model, inputs, outputs):
         ob, oh, ow, oc = model.output_shape[0]
-        scale = model.output_scale[0]
-        t = quantize(model, self.threshold)
+        s = model.output_scale[0]
+        zp = model.output_zero_point[0]
 
         # Reshape the output to a 2D array
         row_outputs = outputs[0].reshape((oh * ow, oc))
 
         # Threshold all the scores
         score_indices = row_outputs[:, _FOMO_CLASSES:]
-        score_indices = threshold(score_indices, t, scale, find_max=True, find_max_axis=1)
+        score_indices = uml.threshold(score_indices, s, zp, self.threshold)
         if not len(score_indices):
             return _NO_DETECTION
 
@@ -157,8 +153,8 @@ class yolo_v2_postprocess:
             return e_x / np.sum(e_x, axis=1, keepdims=True)
 
         ob, oh, ow, oc = model.output_shape[0]
-        scale = model.output_scale[0]
-        t = quantize(model, logit(self.threshold))
+        s = model.output_scale[0]
+        zp = model.output_zero_point[0]
         class_count = (oc // self.anchors_len) - _YOLO_V2_CLASSES
 
         # Reshape the output to a 2D array
@@ -166,7 +162,7 @@ class yolo_v2_postprocess:
 
         # Threshold all the scores
         score_indices = row_outputs[:, _YOLO_V2_SCORE]
-        score_indices = threshold(score_indices, t, scale)
+        score_indices = uml.threshold(score_indices, s, zp, logit(self.threshold))
         if not len(score_indices):
             return _NO_DETECTION
 
@@ -235,8 +231,8 @@ class yolo_v5_postprocess:
 
     def __call__(self, model, inputs, outputs):
         oh, ow, oc = model.output_shape[0]
-        scale = model.output_scale[0]
-        t = quantize(model, self.threshold)
+        s = model.output_scale[0]
+        zp = model.output_zero_point[0]
         class_count = oc - _YOLO_V5_CLASSES
 
         # Reshape the output to a 2D array
@@ -244,7 +240,7 @@ class yolo_v5_postprocess:
 
         # Threshold all the scores
         score_indices = row_outputs[:, _YOLO_V5_SCORE]
-        score_indices = threshold(score_indices, t, scale)
+        score_indices = uml.threshold(score_indices, s, zp, self.threshold)
         if not len(score_indices):
             return _NO_DETECTION
 
@@ -291,8 +287,8 @@ class yolo_v8_postprocess:
 
     def __call__(self, model, inputs, outputs):
         oh, ow, oc = model.output_shape[0]
-        scale = model.output_scale[0]
-        t = quantize(model, self.threshold)
+        s = model.output_scale[0]
+        zp = model.output_zero_point[0]
         class_count = ow - _YOLO_V8_CLASSES
 
         # Reshape the output to a 2D array
@@ -300,7 +296,7 @@ class yolo_v8_postprocess:
 
         # Threshold all the scores
         score_indices = row_outputs[:, _YOLO_V8_CLASSES:]
-        score_indices = threshold(score_indices, t, scale, find_max=True, find_max_axis=1)
+        score_indices = uml.threshold(score_indices, s, zp, self.threshold)
         if not len(score_indices):
             return _NO_DETECTION
 
